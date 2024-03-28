2131161dc4f347648d790fb1b195a155014092a7
JavaTecj9 <joseph.mchugh@volvo.com>
2024-03-27 23:44:08 -0400

    Second Commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,27 @@
+import java.io.*;
+import java.util.*;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
+    public static void main(String[] args) throws IOException {
+
+        String filePath = null;
+
+
+
+        List<String> readInLines = new ArrayList<>();
+
+        FileReader readFile = new FileReader("random.csv");
+        BufferedReader read = new BufferedReader(readFile);
+        String row = null;
+        while ((row = read.readLine()) != null){
+            System.out.println(row);
+        }
+
+
+
+
 
         }
     }
